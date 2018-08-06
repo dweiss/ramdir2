c3bd24cfcea294d3ddea532e4b06968c00495ad4
Dawid Weiss <dawid.weiss@carrotsearch.com>
2018-08-06 12:37:25 +0200

    Moved to a branch.

--- src/test/java/org/apache/lucene/store/ThrowingBiFunction.java
+++
@@ -1,6 +0,0 @@
-package org.apache.lucene.store;
-
-@FunctionalInterface
-interface ThrowingBiFunction<T, U, R> {
-  R apply(T t, U u) throws Exception;
-}
--- src/test/java/org/apache/lucene/store/ThrowingConsumer.java
+++
@@ -1,6 +0,0 @@
-package org.apache.lucene.store;
-
-@FunctionalInterface
-interface ThrowingConsumer<T> {
-    void accept(T t) throws Exception;
-}
